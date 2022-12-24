2ccc0c9be20903747e21ceb4a48ad9091e7a4e04
Heshan <mheshan27@gmail.com>
2022-12-24 21:12:38 +0530

    Add controllers

---
+++ server/src/controller/category.controller.js
@@ -0,0 +1,19 @@
+const Category = require("../models/category.model");
+
+const GetAllCategories = async (req, res) => {};
+
+const GetCategoryById = async (req, res) => {};
+
+const CreateCategory = async (req, res) => {};
+
+const UpdateCategory = async (req, res) => {};
+
+const DeleteCategory = async (req, res) => {};
+
+module.exports = {
+  GetAllCategories,
+  GetCategoryById,
+  CreateCategory,
+  UpdateCategory,
+  DeleteCategory,
+};
