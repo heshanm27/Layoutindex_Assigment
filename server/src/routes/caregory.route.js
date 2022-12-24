1849c459d0cfb366a36f7204af2d3b7142744cb1
Heshan <mheshan27@gmail.com>
2022-12-24 21:15:19 +0530

    Update caregory.route.js

---
+++ server/src/routes/caregory.route.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const { GetAllCategories, GetCategoryById, CreateCategory, UpdateCategory, DeleteCategory } = require("../controller/category.controller");
+
+router.route("/").get(GetAllCategories).post(CreateCategory);
+router.route("/:id").get(GetCategoryById).patch(UpdateCategory).delete(DeleteCategory);
+
+module.exports = router;
