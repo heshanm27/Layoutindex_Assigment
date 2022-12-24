4b20875f7b42415c45fb3617c6fdc5d91eefb812
Heshan <mheshan27@gmail.com>
2022-12-25 00:13:11 +0530

    Update prdouct.controller.js

--- server/src/controller/prdouct.controller.js
+++ server/src/controller/prdouct.controller.js
@@ -1,14 +1,32 @@
 const Product = require("../model/product.model");
 
-const GetAllProducts = async (req, res) => {};
+const GetAllProducts = async (req, res) => {
+  const products = await Product.find({
+    productCategory: req.params.category,
+  });
+  res.status(200).json({ products });
+};
 
-const GetProductById = async (req, res) => {};
+const GetProductById = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  res.status(200).json({ product });
+};
 
-const CreateProduct = async (req, res) => {};
+const CreateProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({ product });
+};
 
-const UpdateProduct = async (req, res) => {};
+const UpdateProduct = async (req, res) => {
+  const product = await Product.findByIdAndUpdate(req.params.id, req.body);
 
-const DeleteProduct = async (req, res) => {};
+  res.status(200).json({ product });
+};
+
+const DeleteProduct = async (req, res) => {
+  const product = await Product.findByIdAndDelete(req.params.id);
+  res.status(200).json({ product });
+};
 
 module.exports = {
   GetAllProducts,
