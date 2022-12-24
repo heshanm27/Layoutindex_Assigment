68d342bf63c985b36b68ce84056c45f88a1f9092
Heshan <mheshan27@gmail.com>
2022-12-25 00:12:56 +0530

    Update category middleware

--- server/src/model/category.model.js
+++ server/src/model/category.model.js
@@ -1,26 +1,29 @@
 const mongoose = require("mongoose");
 
-const CategorySchema = new mongoose.Schema({
-  categoryName: {
-    type: String,
-    required: true,
-    trim: true,
+const CategorySchema = new mongoose.Schema(
+  {
+    categoryName: {
+      type: String,
+      required: true,
+      trim: true,
 
-    index: {
-      unique: true,
-      collation: { locale: "en", strength: 2 },
+      index: {
+        unique: true,
+        collation: { locale: "en", strength: 2 },
+      },
     },
-  },
-  parent: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Category",
-  },
-  sub: [
-    {
+    parent: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Category",
     },
-  ],
-});
+    sub: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Category", CategorySchema);
--- server/src/routes/category.route.js
+++ server/src/routes/category.route.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 const { GetAllCategories, GetCategoryById, CreateCategory, CreateSubCategory, UpdateCategory, DeleteCategory } = require("../controller/category.controller");
+const CheckID = require("../middleware/checkID.middleware");
 
 router.route("/").get(GetAllCategories).post(CreateCategory);
+router.use("/:id", CheckID);
 router.route("/:id").get(GetCategoryById).post(CreateSubCategory).patch(UpdateCategory).delete(DeleteCategory);
 
 module.exports = router;
