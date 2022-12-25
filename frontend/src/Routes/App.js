1be13041dbb9f187b6b9840c15b44489ba079aff
Heshan <mheshan27@gmail.com>
2022-12-25 12:48:27 +0530

    Update App.js

--- frontend/src/Routes/App.js
+++ frontend/src/Routes/App.js
@@ -1,14 +1,12 @@
 import { Route, Router, Routes } from "react-router";
 import Home from "../Pages/Home";
 import Products from "../Pages/Products";
-import ResponsiveDrawer from "../Components/Darwer";
+import ResponsiveDrawer from "../Components/Drawer/Darwer";
 
 function App() {
   return (
     <Routes>
-      <Route element={<ResponsiveDrawer />}>
-        <Route path="/" element={<Home />} />
-      </Route>
+      <Route path="/" element={<Home />} />
       <Route path="/:id" element={<Products />} />
     </Routes>
   );
