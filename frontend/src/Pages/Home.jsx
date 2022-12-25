dcd22e144ea7e6ddf907e02e924075467680c8d2
Heshan <mheshan27@gmail.com>
2022-12-25 13:59:53 +0530

    Update Home.jsx

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ResponsiveDrawer from "../Components/Drawer/Darwer";
 import { Box, Container } from "@mui/material";
+import CustomCard from "../Components/Card/Card";
 
 export default function Home() {
   const [products, setProduct] = React.useState([]);
@@ -8,16 +9,10 @@ export default function Home() {
   return (
     <>
       <ResponsiveDrawer setProduct={setProduct} />
-      <Container sx={{ margin: { sm: "240px" }, padding: "10px" }}>
-        <h1>Hellow</h1>
-        {products &&
-          products.map((product, index) => (
-            <div key={index}>
-              <h1>{product.productName}</h1>
-              <p>{product.productDescription}</p>
-              <p>{product.productPrice}</p>
-            </div>
-          ))}
+      <Container sx={{ marginLeft: { sm: "240px" }, marginTop: "50px", padding: "10px" }}>
+        <Box sx={{ display: "flex", justifyContent: { xs: "center", sm: "start" } }}>
+          {products && products.map((product, index) => <CustomCard item={product} key={product._id} />)}
+        </Box>
       </Container>
     </>
   );
