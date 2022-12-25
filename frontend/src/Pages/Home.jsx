7cb41c14205cd009be550e067dfd9d55efab8a91
Heshan <mheshan27@gmail.com>
2022-12-25 21:44:07 +0530

    Update Home.jsx

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,28 +1,46 @@
 import React from "react";
 import ResponsiveDrawer from "../Components/Drawer/Darwer";
-import { Box, Container, Grid, Stack } from "@mui/material";
+import { Box, CircularProgress, Container, Grid, Stack } from "@mui/material";
 import CustomCard from "../Components/Card/Card";
 import { useEffect } from "react";
+import { FetchContext } from "../Contexts/FeatchContext";
 
 export default function Home() {
   const [products, setProduct] = React.useState([]);
-
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(false);
   useEffect(() => {}, [products]);
   return (
     <>
-      <ResponsiveDrawer setProduct={setProduct} />
-
-      <Box sx={{ marginLeft: { sm: "240px" }, marginTop: "50px", display: "flex", justifyContent: "center", alignItems: "center", p: 2 }}>
-        <Grid container spacing={5} direction="row">
-          {products &&
-            products.map((product, index) => (
-              <Grid item xs={12} sm={12} md={6} lg={4} xl={3} sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-                <CustomCard item={product} key={product._id} />
+      <FetchContext.Provider
+        value={{
+          setProduct,
+          loading,
+          setLoading,
+          error,
+          setError,
+        }}
+      >
+        {loading ? (
+          <Stack direction="row" justifyContent="center" alignItems="center">
+            <CircularProgress />
+          </Stack>
+        ) : (
+          <Box>
+            <ResponsiveDrawer />
+            <Box sx={{ marginLeft: { sm: "240px" }, marginTop: "50px", display: "flex", justifyContent: "center", alignItems: "center", p: 2 }}>
+              <Grid container spacing={5} direction="row">
+                {products &&
+                  products.map((product, index) => (
+                    <Grid item xs={12} sm={12} md={6} lg={4} xl={3} sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                      <CustomCard item={product} key={product._id} />
+                    </Grid>
+                  ))}
               </Grid>
-            ))}
-        </Grid>
-        \
-      </Box>
+            </Box>
+          </Box>
+        )}
+      </FetchContext.Provider>
     </>
   );
 }
