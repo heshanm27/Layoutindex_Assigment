23cf0a63b985e28427ffc337753e52be0c81bbee
Heshan <mheshan27@gmail.com>
2022-12-25 21:15:17 +0530

    Update Home.jsx

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import ResponsiveDrawer from "../Components/Drawer/Darwer";
 import { Box, Container, Grid, Stack } from "@mui/material";
 import CustomCard from "../Components/Card/Card";
+import { useEffect } from "react";
 
 export default function Home() {
   const [products, setProduct] = React.useState([]);
 
+  useEffect(() => {}, [products]);
   return (
     <>
       <ResponsiveDrawer setProduct={setProduct} />
 
-      <Box sx={{ marginLeft: { sm: "240px" }, marginTop: "50px", display: "flex", justifyContent: "center", alignItems: "center", p: 5 }}>
+      <Box sx={{ marginLeft: { sm: "240px" }, marginTop: "50px", display: "flex", justifyContent: "center", alignItems: "center", p: 2 }}>
         <Grid container spacing={5} direction="row">
           {products &&
             products.map((product, index) => (
-              <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={3} sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
                 <CustomCard item={product} key={product._id} />
               </Grid>
             ))}
