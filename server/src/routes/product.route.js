32a135f9e1dd8052e551c28fca0604f710964611
Heshan <mheshan27@gmail.com>
2022-12-25 09:29:43 +0530

    Update product.route.js

--- server/src/routes/product.route.js
+++ server/src/routes/product.route.js
@@ -4,8 +4,8 @@ const CheckID = require("../middleware/checkID.middleware");
 const Upload = require("../middleware/uploadImages.middleware");
 const { GetAllProducts, GetProductById, CreateProduct, UpdateProduct, DeleteProduct } = require("../controller/prdouct.controller");
 
-router.route("/").get(GetAllProducts).post(Upload.array("productImage", 4), CreateProduct);
+router.route("/").get(GetAllProducts).post(Upload.single("productImage"), CreateProduct);
 router.use("/:id", CheckID);
-router.route("/:id").get(GetProductById).patch(UpdateProduct).delete(DeleteProduct);
+router.route("/:id").get(GetProductById).patch(Upload.single("productImage"), UpdateProduct).delete(DeleteProduct);
 
 module.exports = router;
