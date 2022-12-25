2b4687fdc79731ff577ff7ae1c01def1339edb2f
Heshan <mheshan27@gmail.com>
2022-12-26 01:12:34 +0530

    Create SnackBar.jsx

---
+++ frontend/src/Components/SnackBar/SnackBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Alert, AlertTitle } from "@mui/material";
+import { Snackbar } from "@mui/material";
+
+export default function CustomSnackBar({ notify, setNotify }) {
+  const handleClose = () => {
+    setNotify({
+      ...notify,
+      isOpen: false,
+    });
+  };
+
+  return (
+    <Snackbar open={notify.isOpen} autoHideDuration={5000} onClose={handleClose}>
+      <Alert severity={notify ? notify.type : "info"} onClose={handleClose}>
+        <AlertTitle>{notify.title}</AlertTitle>
+        {notify.message}
+      </Alert>
+    </Snackbar>
+  );
+}
