eb755093cd17fbd542e10c144babe1089aef4276
Heshan <mheshan27@gmail.com>
2022-12-26 01:12:18 +0530

    Update category.route.js

--- server/src/routes/category.route.js
+++ server/src/routes/category.route.js
@@ -1,10 +1,19 @@
 const express = require("express");
 
 const router = express.Router();
-const { GetAllCategories, GetSubCategories, CreateCategory, CreateSubCategory, UpdateCategory, DeleteCategory } = require("../controller/category.controller");
+const {
+  GetAllCategories,
+  GetAllSubCategories,
+  GetSubCategories,
+  CreateCategory,
+  CreateSubCategory,
+  UpdateCategory,
+  DeleteCategory,
+} = require("../controller/category.controller");
 const CheckID = require("../middleware/checkID.middleware");
 
 router.route("/").get(GetAllCategories).post(CreateCategory);
+router.route("/sub").get(GetAllSubCategories);
 router.use("/:id", CheckID);
 router.route("/:id/sub").get(GetSubCategories);
 router.route("/:id").post(CreateSubCategory).patch(UpdateCategory).delete(DeleteCategory);
