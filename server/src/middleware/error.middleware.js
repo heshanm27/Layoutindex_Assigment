f75aa9c5edef954e4c2a9d4bebbfa34df2c0fdcc
Heshan <mheshan27@gmail.com>
2022-12-25 10:32:14 +0530

    Update error.middleware.js

--- server/src/middleware/error.middleware.js
+++ server/src/middleware/error.middleware.js
@@ -2,10 +2,11 @@ const CustomError = require("../error/custom.error.js");
 
 const ErrorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomError) {
-    return res.status(err.status).json({ error: err.message });
+    return res.status(err.status).json({ status: error, error: err.message });
   }
 
   return res.status(500).json({
+    status: "error",
     error: err.message,
     msg: "Something went wrong, please try again later",
   });
