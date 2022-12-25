ff9d49889cab3f2e07926b14cfbbe1bc123ca653
Heshan <mheshan27@gmail.com>
2022-12-25 13:27:16 +0530

    Update DrawerContent.jsx

--- frontend/src/Components/Drawer/DrawerContent.jsx
+++ frontend/src/Components/Drawer/DrawerContent.jsx
@@ -1,59 +1,59 @@
-import { Collapse, Divider, List, ListItem, ListItemButton, ListItemText, Radio, Toolbar } from "@mui/material";
+import { Box, Collapse, Divider, List, ListItem, ListItemButton, ListItemText, Radio, Toolbar } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { AxiosRequest } from "../../Utils/DefaultAxios";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 
-export default function DrawerContent() {
-  const [selectedCategory, setSelectedCategory] = useState("");
+export default function DrawerContent({ setProduct }) {
   const [categorIes, setCategories] = useState([]);
-
   const [openCategory, setOpenCategory] = useState("");
-  const handleClick = (id) => {
-    setOpenCategory(id);
-    console.log(id);
+
+  const handleClick = async (id) => {
+    setOpenCategory((prev) => (prev !== id ? id : ""));
+    const { data } = await AxiosRequest.get(`/product`);
+    console.log(data);
+    setProduct(data.data);
   };
 
   const hadnleClickSubCategory = (aprentId, subId) => {
     console.log(aprentId, subId);
   };
 
-  const FetchCategory = async () => {
-    try {
-      const { data } = await AxiosRequest.get("/category");
-      setCategories(data.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   useEffect(() => {
-    FetchCategory();
+    (async () => {
+      try {
+        const { data } = await AxiosRequest.get("/category");
+        setCategories(data.data);
+        handleClick(data.data[0]._id);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
   }, []);
 
   return (
     <div>
       <Toolbar />
       <Divider />
-      <List>
+      <List key={5}>
         {categorIes &&
-          categorIes.map((category) => (
-            <>
-              <ListItemButton onClick={() => handleClick(category._id)} key={category._id}>
+          categorIes.map((category, index) => (
+            <Box key={index}>
+              <ListItemButton onClick={() => handleClick(category._id)}>
                 <ListItemText primary={category.categoryName} />
                 {category.sub && openCategory ? <ExpandLess /> : <ExpandMore />}
               </ListItemButton>
-              <Collapse in={openCategory === category._id} timeout="auto" unmountOnExit>
+              <Collapse in={openCategory === category._id} timeout="auto" unmountOnExit key={category._id + category.sub._id}>
                 <List component="ul">
                   {category.sub &&
-                    category.sub.map((subCategory) => (
-                      <ListItemButton onClick={() => hadnleClickSubCategory(category._id, subCategory._id)} key={subCategory._id}>
+                    category.sub.map((subCategory, index) => (
+                      <ListItemButton onClick={() => hadnleClickSubCategory(category._id, subCategory._id)} key={subCategory._id + index}>
                         <ListItemText primary={subCategory.categoryName} />
                       </ListItemButton>
                     ))}
                 </List>
               </Collapse>
-            </>
+            </Box>
           ))}
       </List>
       <Divider />
