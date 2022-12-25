74a3f8633d896028b6713a32bd46d8f8512702c4
Heshan <mheshan27@gmail.com>
2022-12-25 19:19:21 +0530

    Update Card.jsx

--- frontend/src/Components/Card/Card.jsx
+++ frontend/src/Components/Card/Card.jsx
@@ -1,15 +1,18 @@
 import { Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
 import React from "react";
+import CustomeDialog from "../Dialog/CustomDialog";
+import ProductFrom from "../Form/ProductFrom";
 
 export default function CustomCard({ item }) {
   const formatter = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
   });
+  const [openproductDialog, setOpenProductDialog] = React.useState(false);
 
   return (
     <Card sx={{ width: 285 }}>
-      <CardActionArea>
+      <CardActionArea onClick={() => setOpenProductDialog(true)}>
         <CardMedia
           component="img"
           width={194}
@@ -31,6 +34,10 @@ export default function CustomCard({ item }) {
           </Typography>
         </CardContent>
       </CardActionArea>
+
+      <CustomeDialog title={"Update  Product"} open={openproductDialog} setOpen={setOpenProductDialog}>
+        <ProductFrom setOpen={setOpenProductDialog} existingProduct={item} />
+      </CustomeDialog>
     </Card>
   );
 }
