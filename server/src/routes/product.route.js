3158073758607209b15a68085420c80d762bde4c
Heshan <mheshan27@gmail.com>
2022-12-25 10:32:09 +0530

    Update product.route.js

--- server/src/routes/product.route.js
+++ server/src/routes/product.route.js
@@ -2,9 +2,18 @@ const express = require("express");
 const router = express.Router();
 const CheckID = require("../middleware/checkID.middleware");
 const Upload = require("../middleware/uploadImages.middleware");
-const { GetAllProducts, GetProductById, CreateProduct, UpdateProduct, DeleteProduct } = require("../controller/prdouct.controller");
+const {
+  GetParentCategoryProducts,
+  GetSubCategoryProducts,
+  GetProductById,
+  CreateProduct,
+  UpdateProduct,
+  DeleteProduct,
+} = require("../controller/prdouct.controller");
 
-router.route("/").get(GetAllProducts).post(Upload.single("productImage"), CreateProduct);
+router.route("/:parent").get(GetParentCategoryProducts);
+router.route("/:parent/:sub").get(GetSubCategoryProducts);
+router.route("/").post(Upload.single("productImage"), CreateProduct);
 router.use("/:id", CheckID);
 router.route("/:id").get(GetProductById).patch(Upload.single("productImage"), UpdateProduct).delete(DeleteProduct);
 
