caa4e61b24b95862ec5e80433f2b8b6da945c775
Heshan <mheshan27@gmail.com>
2022-12-25 17:19:04 +0530

    Update ProductFrom.jsx

--- frontend/src/Components/Form/ProductFrom.jsx
+++ frontend/src/Components/Form/ProductFrom.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Chip, Container, FormControl, Input, InputLabel, MenuItem, Select, TextField } from "@mui/material";
+import { Box, Button, Chip, Container, FormControl, FormHelperText, Input, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 import { Stack } from "@mui/system";
 import React, { useState } from "react";
 import { AxiosRequest } from "../../Utils/DefaultAxios";
@@ -10,6 +10,7 @@ const initProduct = {
   productDescription: "",
   productCategory: "",
   productSubCategory: [],
+  productImage: "",
 };
 
 export default function ProductFrom() {
@@ -18,7 +19,6 @@ export default function ProductFrom() {
   const [image, setImage] = useState(null);
   const [mainCategorey, setMainCategorey] = useState([]);
   const [subCategoreies, setSubCategoreies] = useState();
-  const [subCategorey, setSubCategorey] = useState([]);
   const [imageUrl, setImageUrl] = useState(null);
 
   useEffect(() => {
@@ -28,6 +28,7 @@ export default function ProductFrom() {
     })();
   }, []);
 
+  //handle input changes
   const handlehanges = (e) => {
     const { name, value } = e.target;
     console.log(name, value);
@@ -37,12 +38,14 @@ export default function ProductFrom() {
     }));
   };
 
+  //Validate form
   const validate = () => {
     let temp = {};
     temp.productName = product.productName === "" ? "Please enter product name" : "";
     temp.productPrice = (product.productPrice === "" ? "Please product price" : "") || (product.productPrice < 0 ? "Please enter valid price" : "");
     temp.productDescription = product.productDescription === "" ? "Please enter product discription" : "";
-
+    temp.productCategory = product.productCategory === "" ? "Please select product category" : "";
+    temp.productSubCategory = product.productSubCategory.length === 0 ? "Please select product sub category" : "";
     setErrors({
       ...temp,
     });
@@ -50,9 +53,15 @@ export default function ProductFrom() {
     return Object.values(temp).every((x) => x === "");
   };
 
+  //handle image change
   const handleImageChange = (event) => {
     const file = event.target.files[0];
-    setImage(file);
+    // setImage(file);
+
+    setProduct((prev) => ({
+      ...prev,
+      productImage: file,
+    }));
 
     const reader = new FileReader();
     reader.onload = () => {
@@ -61,29 +70,41 @@ export default function ProductFrom() {
     reader.readAsDataURL(file);
   };
 
+  //handle main categorey change
   const handleMainCategoreyChange = async (event) => {
-    // setMainCategorey(event.target.value);
     setProduct((prev) => ({
       ...prev,
       productCategory: event.target.value,
     }));
     const { data } = await AxiosRequest.get(`category/${event.target.value}/sub`);
-    console.log(data.data);
     setSubCategoreies(data.data.sub);
   };
 
-  const handleSubCategoreyChange = (event) => {};
+  //handle sub categorey change
+  const handleSubCategoreyChange = (event) => {
+    setProduct((prev) => ({
+      ...prev,
+      productSubCategory: [...prev.productSubCategory, event.target.value],
+    }));
+  };
+
+  //handle submit
   const handleSubmit = (event) => {
     event.preventDefault();
     if (validate()) {
+      const formData = new FormData();
+      formData.append("productName", product.productName);
+      formData.append("productPrice", product.productPrice);
+      formData.append("productDescription", product.productDescription);
+      formData.append("productCategory", product.productCategory);
+      formData.append("productSubCategory", product.productSubCategory);
+      formData.append("productImage", product.productImage);
+
+      for (var pair of formData.entries()) {
+        console.log(pair[0] + ", " + pair[1]);
+      }
+      console.log(formData);
     }
-    console.log(event);
-    const formData = new FormData();
-    // formData.append("name", name);
-    // formData.append("description", description);
-    // formData.append("price", price);
-    // formData.append("image", image);
-    console.log(formData);
 
     // axios.post('/api/products', formData, {
     //   headers: {
@@ -147,29 +168,34 @@ export default function ProductFrom() {
                   </MenuItem>
                 ))}
             </Select>
+            {error.productCategory && <FormHelperText error>{error.productCategory}</FormHelperText>}
           </FormControl>
 
           <FormControl>
-            <InputLabel id="category-label">Sub Category</InputLabel>
+            <InputLabel id="category-label" sx={{ p: -3 }}>
+              Sub Category
+            </InputLabel>
             <Select
               labelId="category-label"
-              value={subCategorey}
+              multiple
+              value={product.productSubCategory}
               onChange={handleSubCategoreyChange}
               renderValue={(selected) => (
                 <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
                   {selected.map((value) => (
-                    <Chip key={value} label={value.categoryName} />
+                    <Chip key={value} label={value} />
                   ))}
                 </Box>
               )}
             >
               {subCategoreies &&
                 subCategoreies.map((item) => (
-                  <MenuItem key={item._id} value={item}>
+                  <MenuItem key={item._id} value={item.categoryName}>
                     {item.categoryName}
                   </MenuItem>
                 ))}
             </Select>
+            {error.productSubCategory ? <FormHelperText error>{error.productSubCategory}</FormHelperText> : null}
           </FormControl>
 
           <div>
@@ -182,6 +208,7 @@ export default function ProductFrom() {
                 inputProps={{ accept: "image/jpg, image/png, image/jpeg" }}
                 onChange={handleImageChange}
               />
+              {error.productImage ? <FormHelperText error>{error.productImage}</FormHelperText> : null}
             </FormControl>
             {imageUrl ? <img height="25%" width="100%" src={imageUrl} alt="Selected image" /> : null}
           </div>
