ba103f55e8d9afe6b85586027096c3f50d88bce1
Heshan <mheshan27@gmail.com>
2022-12-25 13:27:18 +0530

    Update Home.jsx

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ResponsiveDrawer from "../Components/Drawer/Darwer";
+import { Box, Container } from "@mui/material";
 
 export default function Home() {
   const [products, setProduct] = React.useState([]);
@@ -7,6 +8,17 @@ export default function Home() {
   return (
     <>
       <ResponsiveDrawer setProduct={setProduct} />
+      <Container sx={{ margin: { sm: "240px" }, padding: "10px" }}>
+        <h1>Hellow</h1>
+        {products &&
+          products.map((product, index) => (
+            <div key={index}>
+              <h1>{product.productName}</h1>
+              <p>{product.productDescription}</p>
+              <p>{product.productPrice}</p>
+            </div>
+          ))}
+      </Container>
     </>
   );
 }
