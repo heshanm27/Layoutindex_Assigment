32590e7a9a060cd2b55e48c3813fb8271c31337e
Heshan <mheshan27@gmail.com>
2022-12-25 17:02:39 +0530

    Create CategoryForm.jsx

---
+++ frontend/src/Components/Form/CategoryForm.jsx
@@ -0,0 +1,33 @@
+// import { Box, FormControl, InputLabel, MenuItem, Select, Tab, Tabs } from "@mui/material";
+// import { Container } from "@mui/system";
+// import React, { useState } from "react";
+// import { useEffect } from "react";
+// import { AxiosRequest } from "../../Utils/DefaultAxios";
+
+export function CategoreySubForm() {
+  // useEffect(() => {
+  //   (async () => {
+  //     const { data } = await AxiosRequest.get("/categorey");
+  //     setCategorey(data.data);
+  //   })();
+  // }, []);
+  return (
+    <></>
+    // <>
+    //   <form>
+    //     {/* <FormControl fullWidth>
+    //       <InputLabel id="demo-simple-select-label">Select Parent Categorey</InputLabel>
+    //       <Select labelId="demo-simple-select-label" id="demo-simple-select" label="Select Parent Categorey">
+    //         <MenuItem value={10}>Ten</MenuItem>
+    //         <MenuItem value={20}>Twenty</MenuItem>
+    //         <MenuItem value={30}>Thirty</MenuItem>
+    //       </Select>
+    //     </FormControl> */}
+    //   </form>
+    // </>
+  );
+}
+
+export default function CategoryForm() {
+  return <></>;
+}
